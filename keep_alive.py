bde8a15fe4f6eef30eafbdd695c4666933d911c4
efut-coder <evinokurova@gmail.com>
2025-07-02 17:41:11 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -6,12 +6,12 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return "Bot is alive!"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True  # ensures Flask doesn’t block your bot
     t.start()
-
